7b3e486a657cee286590f288033f15aa0f1ce35b
Sariszhou <Sariszhou@foxmail.com>
2022-01-30 00:09:45 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="app-container">app</div>
+  <span class="text">text</span>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+});
+</script>
+
+<style lang="scss">
+.text {
+  color: $primary-color;
+}
+</style>
